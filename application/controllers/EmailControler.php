5acce4226573acd8df3a9e4aef814f90846789b1
Hassan <hassi.x.malik@gmail.com>
2024-07-18 22:36:21 +0500

    sheesh

--- application/controllers/EmailControler.php
+++ application/controllers/EmailControler.php
@@ -23,8 +23,8 @@ class EmailControler extends CI_Controller {
             $mail->isSMTP();
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'cprogramer04@gmail.com';
-            $mail->Password   = 'xynfiltuzvlxmlpc'; // Your App Password
+            $mail->Username   = '';
+            $mail->Password   = ''; // Your App Password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port       = 587;
 
